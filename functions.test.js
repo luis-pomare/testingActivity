a8b7a234ee12e3dba149683a885cf0ae4290731b
unknown <luispomare92@gmail.com>
2022-03-18 06:44:11 -0500

    Add test for task 3

--- functions.test.js
+++ functions.test.js
@@ -2,10 +2,8 @@ const { test } = require("@jest/globals");
 const { describe } = require("yargs");
 const functions = require("./functions.js");
 
-describe("String functions", () => {
-  test("Read valid length 1", () => {
-    expect(functions.stringLength("Microverse")).toBe(10);
-  });
+test("Read valid length 1", () => {
+  expect(functions.stringLength("Microverse")).toBe(10);
 });
 
 test("Read valid length 2", () => {
@@ -22,4 +20,52 @@ test("Read unvalid length 2", () => {
 
 test("Read reversed string", () => {
   expect(functions.reverseString("Microverse")).toBe("esrevorciM");
+});
+
+test("Read added number 1", () => {
+  expect(functions.calculator.add(2, 2)).toBe(4);
+});
+
+test("Read added number 2", () => {
+  expect(functions.calculator.add(-1, 0)).toBe(-1);
+});
+
+test("Read added number 3", () => {
+  expect(functions.calculator.add(2, 2)).toBe(4);
+});
+
+test("Read subtracted number 1", () => {
+  expect(functions.calculator.subtract(-1, 0)).toBe(-1);
+});
+
+test("Read subtracted number 2", () => {
+  expect(functions.calculator.subtract(-100, -100)).toBe(0);
+});
+
+test("Read subtracted number 3", () => {
+  expect(functions.calculator.subtract(-100, 100)).toBe(-200);
+});
+
+test("Read multiplied number 1", () => {
+  expect(functions.calculator.multiply(1, 0)).toBe(0);
+});
+
+test("Read multiplied number 2", () => {
+  expect(functions.calculator.multiply(-100, -100)).toBe(10000);
+});
+
+test("Read multiplied number 3", () => {
+  expect(functions.calculator.multiply(-100, 100)).toBe(-10000);
+});
+
+test("Read divided number 1", () => {
+  expect(functions.calculator.divide(-1, 2)).toBe(-0.5);
+});
+
+test("Read divided number 2", () => {
+  expect(functions.calculator.divide(-100, -100)).toBe(1);
+});
+
+test("Read divided number 3", () => {
+  expect(functions.calculator.divide(-100, 100)).toBe(-1);
 });
